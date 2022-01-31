1ebe237c71feb46795a1ee82e23c59e4ac66abe2
Devin <adams.devin.c@gmail.com>
2022-01-30 17:06:48 -0800

    Just changed the file structure

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from util import NBAPI
 
 
 session = NBAPI('127.0.0.1', 'admin', 'password')
-s2xml = open('request.xml', 'r').read()
-login = open('login.xml', 'r').read()
+s2xml = open('XML/request.xml', 'r').read()
+login = open('XML/login.xml', 'r').read()
 
 request = xml.parse(s2xml)
 
